084e6f1dfff149148bc324074e0cc4c2063a9dcf
charlie83xt <charlie83xt@hotmail.com>
2021-10-08 15:57:40 +0100

    Skinny controlers refactored

--- app.rb
+++ app.rb
@@ -1,5 +1,7 @@
 require 'sinatra'
 require 'sinatra/reloader'
+require './lib/game'
+require './lib/player'
 
 
 class Battle < Sinatra::Base
@@ -14,21 +16,20 @@ class Battle < Sinatra::Base
   end
 
   post '/names' do
-    $player_1 = Player.new(params[:player_1_name])
-    $player_2 = Player.new(params[:player_2_name])
+    player_1 = Player.new(params[:player_1_name])
+    player_2 = Player.new(params[:player_2_name])
+    $game = Game.new(player_1, player_2)
     redirect '/play'
   end
   
   get '/play' do
-    @player_1_name = $player_1.name
-    @player_2_name = $player_2.name
+    @game = $game
     erb :play
   end
 
   get '/attack' do
-    @player_1_name = $player_1
-    @player_2_name = $player_2
-    @player_1_name.attack(@player_2_name)
+    @game = $game
+    @game.attack(@player_2_name)
     erb :attack
   end
 
